216d1373cd419eeb462632715f5c71e3b32f92e2
Sophie <53736936+SophieH07@users.noreply.github.com>
2024-12-06 21:58:48 +0100

    add activetab style

--- frontend/src/components/Filter.jsx
+++ frontend/src/components/Filter.jsx
@@ -1,11 +1,36 @@
+import { useState } from "react";
 import { FaArrowDown, FaArrowUp } from "react-icons/fa";
 
 export default function Filter({ fetchBooks, showAddForm, toggleShowAddForm }) {
+  const [activeTab, setActiveTab] = useState(null);
+
+  const setActiveFilter = (filter) => {
+    setActiveTab(filter);
+    fetchBooks(filter);
+  };
+
   return (
     <div id="filter">
-      <button onClick={() => fetchBooks("all")}>All books</button>
-      <button onClick={() => fetchBooks(true)}>Read books</button>
-      <button onClick={() => fetchBooks(false)}>Unread books</button>
+      <button
+        onClick={() => setActiveFilter("all")}
+        className={` ${
+          (activeTab === "all" || activeTab === null) && "active-tab"
+        }`}
+      >
+        All books
+      </button>
+      <button
+        onClick={() => setActiveFilter(true)}
+        className={` ${activeTab === true && "active-tab"}`}
+      >
+        Read books
+      </button>
+      <button
+        onClick={() => setActiveFilter(false)}
+        className={` ${activeTab === false && "active-tab"}`}
+      >
+        Unread books
+      </button>
       <button
         className="inline-flex justify-center items-center gap-4"
         onClick={() => toggleShowAddForm()}
